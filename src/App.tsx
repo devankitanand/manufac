c3e1ee237bf1333831441140104a8089e26fa951
Ankit Anand <devankitanand@gmail.com>
2024-12-04 07:21:26 +0530

    complete project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
-import { theme } from "./theme";
+
+import '@mantine/core/styles.css';
+import './App.css'
+import './components/Yeardata.js';
+import Yeardata from './components/Yeardata.tsx';
+
+import { MantineProvider } from '@mantine/core';
+import Cropdata from './components/Cropdata.tsx';
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  return <MantineProvider>{
+    <>
+    <div className='bodywarp'>
+      
+        <div className='heading'>
+            Manufac App
+          </div>
+          <div className='content'>
+                <Yeardata/>
+                <Cropdata/>
+          </div>
+      
+        
+    </div>
+    </>
+  }</MantineProvider>;
 }
+
+
+
+
